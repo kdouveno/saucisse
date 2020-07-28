12abc8ceb0cec20b3738c6b1b19be190931ca86e
kdouveno <isordee@gmail.com>
2020-07-28 21:04:42 +0200

    the shit works

--- prng.cpp
+++ prng.cpp
@@ -2,17 +2,26 @@
 #include <random>
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <sstream>
 
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	seed_seq seq{45, 1, 3};
-	minstd_rand0 generator(seq);
-
-	uniform_real_distribution distr(0.0, 1.0);
-	cout << distr(generator) << endl;
+	minstd_rand0 generator = minstd_rand0();
+	uniform_real_distribution<float> distr(0.0, 1.0);
+	stringstream stream = stringstream();
+	stream << "[\n";
+	for (int i  = 0; i < 1000; i++){
+		seed_seq seq = {45, i, 3};
+		generator.seed(seq);
+		stream << distr(generator) << ",\n";
+	}
+	ofstream fs = ofstream();
+	fs.open("output.json");
+	fs << stream.str() << "]" << endl;
 
 	return 0;
 }
